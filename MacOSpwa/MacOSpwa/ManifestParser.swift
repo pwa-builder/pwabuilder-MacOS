f7a6af3a92cd4e6c7d4c15fe53636694a876e596
Rumsha7 <rumsha7@gmail.com>
2018-06-26 18:46:04 -0700

    Added more functions + init function to ManifestParser

--- MacOSpwa/MacOSpwa/ManifestParser.swift
+++ MacOSpwa/MacOSpwa/ManifestParser.swift
@@ -16,26 +16,60 @@ class ManifestParser {
     private var appName: String = ""
     private var appURL: String = ""
     private var appDisplay: String = ""
-    private var appThemeColor: String = ""
+    private var appThemeColorHex: String = ""
+    private var appThemeColor: NSColor = NSColor.lightGray
     private var appScope: String = ""
+    
+    // MARK: - Initialize App Properties
+    init(json: [String:Any]){
+        parseManifest(json: json) //initializes with app properties with data from manifest
+        appThemeColor = convertHexToNSColor(hexString: appThemeColorHex) //sets the theme color of the app
+    }
+    
+    private func parseManifest(json: [String:Any]) -> Void {
+        //Xcode throws a runtime error if the json object does not contain a key with the given string
+        appName = json["name"] as! String
+        appURL = json["start_url"] as! String
+        appDisplay = json["display"] as! String
+        appThemeColorHex = json["theme_color"] as! String
+        appScope = json["scope"] as! String
+    }
+    
+    /*
+     Takes a hex string and converts it to NSColor type
+     */
+    private func convertHexToNSColor(hexString: String) -> NSColor {
+        //TODO: Should there be an assert statement instead? Ex: assert(hexString.hasPrefix("#"),"Theme-color format in manifest is invalid. Correct emample format: #4F4F4F")
+        if !hexString.hasPrefix("#") {
+            print("Theme-color format in manifest is invalid. Correct emample format: #4F4F4F. A default color of light grey will be returned")
+            return NSColor.lightGray //return gray as the default
+        }
+        var colorString = hexString
+        colorString.remove(at: colorString.startIndex) //assuming '#' is included in the string at this point
+        
+        //Convert the hex string to a hex number
+        let scanner = Scanner(string: colorString)
+        var hexNumber = UInt32() //Int32 because that's the closest to 24 bits, which is the size of RGB values
+        if scanner.scanHexInt32(&hexNumber) {
+            //To convert hex number to CGFloat: apply a mask (if necessary), shift the bits to the right (if necessary), divide by 255
+            let red = CGFloat(hexNumber >> 16)/255
+            let green = CGFloat((hexNumber & 0x00ff00) >> 8)/255
+            let blue = CGFloat(hexNumber & 0x0000ff)/255
+            return NSColor(red: red, green: green, blue: blue, alpha: 1.0)
+        } else { //given hex value is not valid
+            return NSColor.lightGray //return gray as the default
+        }
+    }
     
     // MARK: - GETTER METHODS
     func getAppName() -> String { return appName }
     func getAppURL() -> String { return appURL }
     func getAppDisplay() -> String { return appDisplay }
-    func getAppThemeColor() -> String { return appThemeColor }
+    func getAppThemeColor() -> NSColor { return appThemeColor }
     func getAppScope() -> String { return appScope }
     
-    // MARK: - MODEL METHODS
-    func parseManifest(json: [String:Any]) -> Void {
-        //Xcode throws a runtime error if the json object does not contain a key with the given string
-        appName = json["name"] as! String
-        appURL = json["start_url"] as! String
-        appDisplay = json["display"] as! String
-        appThemeColor = json["theme_color"] as! String
-        appScope = json["scope"] as! String
-    }
     
+    // MARK: - MODEL METHODS
     func isUrlInManifestScope(url: URL) -> Bool {
         //TODO: check if url valid here?
         if url.absoluteString.hasPrefix("https://" + (url.host)! + appScope){
@@ -63,30 +97,6 @@ class ManifestParser {
         }
     }
     
-    /*
-     Takes a hex string and converts it to NSColor type
-     */
-    func convertHexToNSColor(hexString: String) -> NSColor? {
-        //TODO: Should there be an assert statement instead? Ex: assert(hexString.hasPrefix("#"),"Theme-color format in manifest is invalid. Correct emample format: #4F4F4F")
-        if !hexString.hasPrefix("#") {
-            print("Theme-color format in manifest is invalid. Correct emample format: #4F4F4F. A default color of light grey will be returned")
-            return NSColor.lightGray //return gray as the default
-        }
-        var colorString = hexString
-        colorString.remove(at: colorString.startIndex) //assuming '#' is included in the string at this point
-        
-        //Convert the hex string to a hex number
-        let scanner = Scanner(string: colorString)
-        var hexNumber = UInt32() //Int32 because that's the closest to 24 bits, which is the size of RGB values
-        if scanner.scanHexInt32(&hexNumber) {
-            //To convert hex number to CGFloat: apply a mask (if necessary), shift the bits to the right (if necessary), divide by 255
-            let red = CGFloat(hexNumber >> 16)/255
-            let green = CGFloat((hexNumber & 0x00ff00) >> 8)/255
-            let blue = CGFloat(hexNumber & 0x0000ff)/255
-            return NSColor(red: red, green: green, blue: blue, alpha: 1.0)
-        } else { //given hex value is not valid
-            return nil
-        }
-    }
+    
     
 }
